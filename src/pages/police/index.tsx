e90f29d52918593d1a73f88fc88fa8192bbdba0d
Aniruddha Upadhya K <aniupadhyak1234@gmail.com>
2024-03-15 17:27:14 +0530

    feat: added user dashboard

--- src/pages/police/index.tsx
+++ src/pages/police/index.tsx
@@ -10,29 +10,76 @@ import {
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useEffect, useState } from "react";
+import { DateTimePickerDemo } from "@/components/ui/DateTimePicker/DateTimePicker";
+import { Calendar } from "@/components/ui/calendar";
+import { TimePicker } from "@/components/ui/DateTimePicker/timePicker";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { CalendarIcon } from "lucide-react";
+import { format } from "date-fns";
 
 export default function TabsDemo() {
+  const [lat, setLat] = useState<number>(0);
+  const [long, setLong] = useState<number>(0);
+
+  const [date, setDate] = useState<Date | undefined>();
+
   // axios req to /police/bills
   const data = [
     {
       vehicleNum: "KA-01-MC-1234",
+      crime: "Traffic light skip",
       fine: 1000,
     },
     {
       vehicleNum: "KA-01-MC-1235",
+      crime: "Overspeed",
       fine: 2000,
     },
   ];
+
+  // axios req to /police/enquire
+  const [enquireFetch, setEnquireFetch] = useState(false);
+  const [enquireData, setEnquireData] = useState<
+    { vehicleNum: string; location: string }[]
+  >([]);
+
+  useEffect(() => {
+    if (!enquireFetch) return;
+    // axios req to /police/enquire
+    setEnquireData([
+      {
+        vehicleNum: "KA-01-MC-1234",
+        location: "12.971615° N, 77.594611° E",
+      },
+      {
+        vehicleNum: "KA-01-MC-1235",
+        location: "12.971616° N, 77.594610° E",
+      },
+    ]);
+    setEnquireFetch(false);
+  }, [enquireFetch]);
   return (
     <div className="min-h-screen flex justify-center items-center">
       <Tabs defaultValue="Bills" className="w-[800px]">
@@ -61,6 +108,7 @@ export default function TabsDemo() {
                   {data.map((bill, index) => (
                     <TableRow key={index}>
                       <TableCell className="">{bill.vehicleNum}</TableCell>
+                      <TableCell className="">{bill.crime}</TableCell>
                       <TableCell className="">{bill.fine}</TableCell>
                       <TableCell className="flex justify-end">
                         <Button>Create Warrant</Button>
@@ -81,15 +129,48 @@ export default function TabsDemo() {
                 Change your enquire here. After saving, youll be logged out.
               </CardDescription>
             </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="flex gap-12 ">
+            <CardContent className="space-y-5">
+              <div className="flex gap-6 ">
                 <div className="space-y-1 w-full">
                   <Label htmlFor="current">Lattitude</Label>
-                  <Input id="current" type="text" inputMode="numeric" />
+                  <Input
+                    id="current"
+                    type="text"
+                    inputMode="numeric"
+                    onSubmit={(e) => {
+                      setLat(Number(e.currentTarget.value));
+                    }}
+                  />
                 </div>
                 <div className="space-y-1 w-full">
                   <Label htmlFor="new">Longitude</Label>
                   <Input id="new" type="text" inputMode="numeric" />
+                </div>
+              </div>
+              <div className="w-full flex justify-between gap-6">
+                <div className="flex flex-col gap-1 w-full">
+                  <Label htmlFor="calendar" className="">
+                    Date
+                  </Label>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button name="calendar" variant="outline">
+                        <CalendarIcon className="mr-2 h-4 w-4" />
+                        {date ? format(date, "PPP") : <span>Pick a date</span>}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                      <Calendar
+                        mode="single"
+                        selected={date}
+                        onSelect={setDate}
+                        initialFocus
+                      ></Calendar>
+                    </PopoverContent>
+                  </Popover>
+                </div>
+                <div className="w-full">
+                  <TimePicker date={date} setDate={setDate}></TimePicker>
                 </div>
               </div>
               <div className="space-y-1">
@@ -98,7 +179,41 @@ export default function TabsDemo() {
               </div>
             </CardContent>
             <CardFooter>
-              <Button>Search Vehicles</Button>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button
+                    className="w-full"
+                    onClick={() => setEnquireFetch(true)}
+                  >
+                    Search Vehicles
+                  </Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Nearby Vehicles</DialogTitle>
+                  </DialogHeader>
+                  <div className="">
+                    <Table>
+                      <TableHeader>
+                        <TableRow>
+                          <TableHead>Vehicle Number</TableHead>
+                          <TableHead>Location</TableHead>
+                        </TableRow>
+                      </TableHeader>
+                      <TableBody>
+                        <TableRow>
+                          <TableCell>KA-01-MC-1234</TableCell>
+                          <TableCell>12.9716° N, 77.5946° E</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>KA-01-MC-1235</TableCell>
+                          <TableCell>12.9716° N, 77.5946° E</TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </div>
+                </DialogContent>
+              </Dialog>
             </CardFooter>
           </Card>
         </TabsContent>
---
+++ src/pages/user/[...slug].tsx
@@ -0,0 +1,91 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useEffect, useState } from "react";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+
+export default function User() {
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    // fetch user data
+    // async function fetchUser() {
+    //   const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
+    //   const data = await res.json();
+    //   setUser(data);
+    //   console.log(data);
+    // }
+    // fetchUser();
+
+    setUser({
+      name: "John Doe",
+      phone: "123-456-7890",
+      bills: [
+        {
+          id: 1,
+          date: "2022-01-01",
+          amount: 100.0,
+        },
+        {
+          id: 2,
+          date: "2022-02-01",
+          amount: 200.0,
+        },
+        {
+          id: 3,
+          date: "2022-03-01",
+          amount: 300.0,
+        },
+      ],
+    });
+  }, []);
+  return (
+    <div className="min-h-screen p-12">
+      <div className="flex flex-col justify-center items-center text-center text-gray-100 text-xl gap-3 p-3">
+        <div className="text-3xl font-bold">Details</div>
+        <div className="flex flex-col justify-center items-center">
+          <div className="flex gap-2 ">
+            <div className="">Name: </div>
+            <div className="">{user?.name}</div>
+          </div>
+          <div className="flex gap-2 ">
+            <div className="">Phone Number: </div>
+            <div className="">{user?.phone}</div>
+          </div>
+        </div>
+      </div>
+      <Separator />
+      <div className="text-black">
+        <Table className="bg-white rounded-lg mt-6 text-left">
+          <TableHeader className="">
+            <TableRow>
+              <TableHead className="">ID</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {user?.bills?.map((bill: any, index: number) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell>{bill.id}</TableCell>
+                  <TableCell>{bill.date}</TableCell>
+                  <TableCell className="">{bill.amount}</TableCell>
+                  <TableCell className="text-right w-1/5">
+                    <Button>Rise Query</Button>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
