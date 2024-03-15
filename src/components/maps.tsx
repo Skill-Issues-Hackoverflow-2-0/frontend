83cd04daa028d0335bd0636cd84e002eb7b49236
Aniruddha Upadhya K <aniupadhyak1234@gmail.com>
2024-03-15 22:31:30 +0530

    messed with the map

--- src/components/maps.tsx
+++ src/components/maps.tsx
@@ -13,19 +13,48 @@ export default function Maps() {
   const [map, setMap] = useState({});
 
   useEffect(() => {
-    if(mapElement.current && mapElement)  {
+    if (mapElement.current && mapElement) {
       mapElement.current.style.overflow = "visible";
       let map = tt.map({
         key: "nG6oY1L34rbTfoLz0D205CrB42a3mf8m",
         container: mapElement.current,
-        center: [mapLongitude, mapLatitude],
-        zoom: mapZoom
-      });    
+        center: [-0.2046175878910219, 51.52327158962092],
+        zoom: mapZoom,
+      });
+
+      map.on("load", () => {
+        map.addLayer({
+          id: "overlay",
+          type: "fill",
+          source: {
+            type: "geojson",
+            data: {
+              properties: {},
+              type: "Feature",
+              geometry: {
+                type: "Polygon",
+                coordinates: [
+                  [
+                    [-0.2046175878910219, 51.52327158962092],
+                    [-0.05355557617221507, 51.53523241868879],
+                    [-0.13045987304786877, 51.46299250930767],
+                  ],
+                ],
+              },
+            },
+          },
+          layout: {},
+          paint: {
+            "fill-color": "#db356c",
+            "fill-opacity": 0.5,
+            "fill-outline-color": "black",
+          },
+        });
+      });
+
       setMap(map);
+      return () => map.remove();
     }
-    // return () => map.remove();
   }, []);
-  return (
-    <div ref={mapElement} className="mapDiv h-screen" /> 
-  );
+  return <div ref={mapElement} className="mapDiv h-screen" />;
 }
